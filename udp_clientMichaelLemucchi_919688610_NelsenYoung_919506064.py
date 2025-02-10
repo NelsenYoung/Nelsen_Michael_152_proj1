0cdce88669a1f36a707e609314fbfde2169ee1c6
Michael Lemucchi <109193002+michaellemucchi@users.noreply.github.com>
2025-02-10 11:41:28 -0800

    implemented client request message

--- udp_clientMichaelLemucchi_919688610_NelsenYoung_919506064.py
+++ udp_clientMichaelLemucchi_919688610_NelsenYoung_919506064.py
@@ -1,13 +1,32 @@
 import socket
+import sys
+import random 
+import string
+import time
 
-UDP_IP = "127.0.0.1"
-UDP_PORT = 5005
-MESSAGE = b"Hello, World!"
+def main():
+    # try to find the current time
+    curr = time.time()
+    # should be the length of the random payload
+    payload_len = int(sys.argv[1]) * 1000000 # convert from bytes to megabytes
+    random_payload = ''.join(random.choices(string.ascii_letters + string.digits, k=payload_len))
+    
+    # combine time and payload data
+    data_packet = str(curr) + random_payload
 
-print("UDP target IP: %s" % UDP_IP)
-print("UDP target port: %s" % UDP_PORT)
-print("message: %s" % MESSAGE)
+    UDP_IP = "127.0.0.1"
+    UDP_PORT = 5005
+    MESSAGE = data_packet.encode("utf-8")
 
-sock = socket.socket(socket.AF_INET, # Internet
-                     socket.SOCK_DGRAM) # UDP
-sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+
+    print("UDP target IP: %s" % UDP_IP)
+    print("UDP target port: %s" % UDP_PORT)
+    print("message: %s" % MESSAGE)
+
+    sock = socket.socket(socket.AF_INET, # Internet
+                        socket.SOCK_DGRAM) # UDP
+    sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+
+
+if __name__ == "__main__":
+    main()
