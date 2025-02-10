3c2c64f1c2650390f1d7fe8f71e8b4834ced4649
Nelsen Young <nelsenyoung@campus-040-161.ucdavis.edu>
2025-02-10 11:42:00 -0800

    updated server with time

--- udp_serverMichaelLemucchi_919688610_NelsenYoung_919506064.py
+++ udp_serverMichaelLemucchi_919688610_NelsenYoung_919506064.py
@@ -1,4 +1,5 @@
 import socket
+import time
 
 UDP_IP = "127.0.0.1"
 UDP_PORT = 5005
@@ -9,4 +10,9 @@ sock.bind((UDP_IP, UDP_PORT))
 
 while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+    cur = time.time()
+    encoded = str(data).encode('utf-8')
+    print("split message: %s" % encoded[:4])
     print("received message: %s" % data)
+    print("recieved on: " + str(cur) +  " which is: " + str(time.ctime(cur)))
+    print("size of time: " + str(len(str(cur))))
