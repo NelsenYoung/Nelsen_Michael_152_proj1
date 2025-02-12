5cc78e9d8441fca2471516f98cf7a0008f7a6ff4
Nelsen Young <nelsenyoung@campus-044-254.ucdavis.edu>
2025-02-12 14:29:32 -0800

    added coding and decoding of the message

--- udp_clientMichaelLemucchi_919688610_NelsenYoung_919506064.py
+++ udp_clientMichaelLemucchi_919688610_NelsenYoung_919506064.py
@@ -12,17 +12,17 @@ def main():
     payload_len = mb_to_send * 1000000 # convert from bytes to megabytes
 
     # generate the random payload
-    random_payload = ''.join(random.choices(string.ascii_letters + string.digits, k=payload_len))
+    random_payload = ''.join(random.choices(string.ascii_letters + string.digits + string.punctuation, k=payload_len))
     
     # combine time and payload data
-    data_packet = str(curr_time) + random_payload
+    data_packet = random_payload
     
 
     UDP_IP = "127.0.0.1" # local host
     UDP_PORT = 5005 # client request port
     
     # separate packets by every 9000 and preface each packet with packet # and host IP
-    fragment_size = 9000 
+    fragment_size = 9000
 
     # iterate through the entirety of the data, account for off by 1 offset
     num_fragments = (int(len(data_packet)) / fragment_size) + 1
@@ -30,12 +30,19 @@ def main():
     for i in range(int(num_fragments)):
 
         # convert the string message into a byte object
-        current_fragment = str(i) + UDP_IP + data_packet[i*fragment_size: fragment_size*(i+1)]
+        if(i == int(num_fragments) - 1):
+            current_fragment = str(i).zfill(5) + UDP_IP + "final packet" +  data_packet[i*fragment_size: fragment_size*(i+1)]
+        elif(i == 0):
+            current_fragment = str(i).zfill(5) + UDP_IP + str(curr_time) + data_packet[i*fragment_size: fragment_size*(i+1)]
+        else:
+            current_fragment = str(i).zfill(5) + UDP_IP + data_packet[i*fragment_size: fragment_size*(i+1)]
+
         MESSAGE = current_fragment.encode("utf-8")
         
         print("UDP target IP: %s" % UDP_IP)
         print("UDP target port: %s" % UDP_PORT)
         print("message: %s" % MESSAGE)
+        print("time_sent: " + str(curr_time))
 
         sock = socket.socket(socket.AF_INET, # Internet
                            socket.SOCK_DGRAM) # UDP
--- udp_serverMichaelLemucchi_919688610_NelsenYoung_919506064.py
+++ udp_serverMichaelLemucchi_919688610_NelsenYoung_919506064.py
@@ -8,11 +8,44 @@ sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 sock.bind((UDP_IP, UDP_PORT))
 
+# We know that the first 2 characters are b' so we can ignore them
+# We know that if the next 4 characters are 0, then the time stamp will come after the IP address
+# We know that if the 12 characters after the IP address are "final packet", then the message is the last packet
+# We know that the time stamp will be 17 characters long
+# So the message structure is as follows: [packet number: 5 chars][IP address: 9 chars ]([time stamp: 17 chars])([final packet: 12 chars])[message]
+cur_time = time.time()
+recieved_time = 0.0
+first_bits_first_packet = "Nelsen"
 while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    cur = time.time()
     encoded = str(data).encode('utf-8')
-    print("split message: %s" % encoded[:4])
-    print("received message: %s" % data)
-    print("recieved on: " + str(cur) +  " which is: " + str(time.ctime(cur)))
-    print("size of time: " + str(len(str(cur))))
+    data_start = 19
+    encoded_str = str(encoded)
+    print(encoded_str[:37])
+
+    #print("split message: %s" % encoded[2:19])
+    #print("received message: %s" % data)
+    print("packet number: " + encoded_str[5:10])
+    print("IP address: " + encoded_str[11:19])
+    print("is Final: " + encoded_str[19:31])
+    if(int(encoded_str[5:10]) == 0):
+        print("time stamp: " + encoded_str[20:35])
+        print("recieved on: " + str(cur_time) +  " which is: " + str(time.ctime(cur_time)))
+        recieved_time = float(encoded_str[20:35])
+        print("recieved time: " + str(time.ctime(recieved_time)))
+        first_bits_first_packet = encoded_str[:51]
+        data_start = 36
+    if(encoded_str[19:31] == "final packet"):
+        print("final packet recieved")
+        cur_time = time.time()
+        data_start = 32
+        # Calculate the throughput only when we have recieved all messeages
+        time_diff = cur_time - recieved_time
+        print("recieved time: " + str(time.ctime(recieved_time)))
+        print("current time: " + str(time.ctime(cur_time)))
+        print("time difference: " + str(time_diff) + " seconds")
+        throughput = ((len(encoded[33:])/1000) + (int(encoded_str[5:10] * 9))) / time_diff
+        print("throughput: " + str(throughput) + " bytes/second")
+    
+    print("first bits: " + first_bits_first_packet)
+    print("data: " + encoded_str[data_start:])
