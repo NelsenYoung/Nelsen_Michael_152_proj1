f441adf39251bff3cfb1b21753b6e74ffa91953c
Nelsen Young <nelsenyoung@Nelsens-MacBook-Pro.local>
2025-02-17 17:28:25 -0800

    reformatted client

--- part2/client_NelsenYoung_919506064_MichaelLemucchi_919688610.py
+++ part2/client_NelsenYoung_919506064_MichaelLemucchi_919688610.py
@@ -3,8 +3,9 @@ import json
 
 HOST = "127.0.0.1"  # The server's hostname or IP address
 PORT = 65432  # The port used by the server
+message = "ping"
 
-data = {server_ip: "127.0.0.1", server_port: 65432, message: "ping"}
+data = {server_ip: HOST, server_port: PORT, message: message}
 json_string = json.dumps(data)
 encoded_data = json_string.encode('utf-8')
 
