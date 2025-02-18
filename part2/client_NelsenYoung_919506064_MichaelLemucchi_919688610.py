1c036f3d7beea2984ce2f815c17c0cb9e48220e1
Nelsen Young <nelsenyoung@Nelsens-MacBook-Pro.local>
2025-02-17 17:24:54 -0800

    finished client with non repeating ping

--- part2/client_NelsenYoung_919506064_MichaelLemucchi_919688610.py
+++ part2/client_NelsenYoung_919506064_MichaelLemucchi_919688610.py
@@ -1,11 +1,16 @@
 import socket
+import json
 
 HOST = "127.0.0.1"  # The server's hostname or IP address
 PORT = 65432  # The port used by the server
 
+data = {server_ip: "127.0.0.1", server_port: 65432, message: "ping"}
+json_string = json.dumps(data)
+encoded_data = json_string.encode('utf-8')
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b"Hello, world")
+    s.sendall(encoded_data)
     data = s.recv(1024)
 
 print(f"Received {data!r}")
